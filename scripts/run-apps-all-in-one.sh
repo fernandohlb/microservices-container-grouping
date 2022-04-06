8b627439ec8fda07cea0296dca6ab5381c792f76
Fernando Buzato <fernandohlb@gmail.com>
2022-04-06 15:44:24 -0300

    ci: add new microservices

--- scripts/run-apps-all-in-one.sh
+++ scripts/run-apps-all-in-one.sh
@@ -7,21 +7,20 @@
 # Start the Carts microservice
 java -jar bin/carts.jar --port=8080 &
   
-# Start the Orders microservice
-java -jar bin/orders.jar --port=8081
+# # Start the Orders microservice
+java -jar bin/orders.jar --port=8081 &
 
-# # Start the Shipping microservice
-#java -jar bin/shipping.jar --port=8082 
-#&
+# # # Start the Shipping microservice
+java -jar bin/shipping.jar --port=8082 &
 
-#Start Catalogue microservice
-#./bin/catalogue -port=8083 &
+# # #Start Catalogue microservice
+./bin/catalogue -port=8083 &
 
-#Start Payment microservice
-#./bin/payment -port=8084 &
+# # #Start Payment microservice
+./bin/payment -port=8084 &
 
-#Start User microservice
-#./bin/user -port=8085
+# #Start User microservice
+./bin/user -port=8085
   
 # Wait for any process to exit
 #wait -n
